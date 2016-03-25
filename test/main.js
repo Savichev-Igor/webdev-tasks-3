60feb7f8a03b50521528dfa257f069d06775d360
Savi <Ultimate95@mail.ru>
2016-03-25 13:25:09 +0500

    fixed

--- test/main.js
+++ test/main.js
@@ -67,54 +67,73 @@ describe('Tests for flow.js', () => {
             cb.should.have.been.calledOnce;
             cb.should.have.been.calledWith(testErr, 1);
         });
+
+        it('Should call 2 async funcs & check right order exectution & call cb once', done => {
+            var funcOne = sinon.spy(cb => {
+                setImmediate(() => {
+                    cb(null, 1);
+                });
+            });
+
+            var funcTwo = sinon.spy((data, cb) => {
+                setImmediate(() => {
+                    data += 1;
+                    cb(null, data);
+                });
+            });
+
+            var cb = sinon.spy(() => {
+                funcOne.should.have.been.calledBefore(funcTwo);
+                funcTwo.should.have.been.calledAfter(funcOne);
+                cb.should.have.been.calledOnce;
+                cb.should.have.been.calledWith(null, 2);
+                done();
+            });
+
+            flow.serial([funcOne, funcTwo], cb);
+        });
     });
 
     describe('Test for parallel', () => {
         it('Should call func once & cb once with args null, [1]', done => {
             var func = sinon.spy(cb => {
-                setTimeout(() => {
+                setImmediate(() => {
                     cb(null, 1);
-                }, 500);
+                });
             });
 
             var cb = sinon.spy(() => {
+                func.should.have.been.calledOnce;
                 cb.should.have.been.calledOnce;
                 cb.should.have.been.calledWith(null, [1]);
                 done();
             });
 
             flow.parallel([func], cb);
-
-            func.should.have.been.calledOnce;
         });
 
         it('Should run 2 funcs asyn once & pass data in cb with args null, [1, 2] once', done => {
             var funcOne = sinon.spy(cb => {
-                setTimeout(() => {
+                setImmediate(() => {
                     cb(null, 1);
-                }, 500);
+                });
             });
 
             var funcTwo = sinon.spy(cb => {
-                setTimeout(() => {
+                setImmediate(() => {
                     cb(null, 2);
-                }, 500);
+                });
             });
 
             var cb = sinon.spy(() => {
+                funcOne.should.have.been.calledOnce;
+                funcTwo.should.have.been.calledOnce;
                 cb.should.have.been.calledOnce;
                 cb.should.have.been.calledWith(null, [1, 2]);
                 done();
             });
 
             flow.parallel([funcOne, funcTwo], cb);
-
-            /* Поскольку функции работают асинхронно из-за forEach, хотелось бы как-то
-               научить тест считать его пройденным как с [1, 2] так и с [2, 1], т.к. мы не знаем,
-               что нам придёт первым в результирующий массив, но я не смог найти как это
-               сделать =( */
-            funcOne.should.have.been.calledOnce;
-            funcTwo.should.have.been.calledOnce;
         });
 
         it('Should run 2 funcs async, get err in 1st func & call once cb with err, 1', done => {
@@ -125,41 +144,39 @@ describe('Tests for flow.js', () => {
             });
 
             var funcTwo = sinon.spy(cb => {
-                setTimeout(() => {
+                setImmediate(() => {
                     cb(null, 2);
-                }, 500);
+                });
             });
 
             var cb = sinon.spy(() => {
+                funcOne.should.have.been.calledOnce;
+                funcTwo.should.not.have.been.calledOnce;
                 cb.should.have.been.calledOnce;
                 cb.should.have.been.calledWith(testErr, 1);
                 done();
             });
 
             flow.parallel([funcOne, funcTwo], cb);
-
-            funcOne.should.have.been.calledOnce;
-            funcTwo.should.not.have.been.calledOnce;
         });
     });
 
     describe('Test for map', () => {
         it('Should call func once & call cb once with args null, [1]', done => {
             var func = sinon.spy((data, cb) => {
-                setTimeout(() => {
+                setImmediate(() => {
                     cb(null, data);
-                }, 500);
+                });
             });
 
             var cb = sinon.spy(() => {
+                func.should.have.been.calledOnce;
                 cb.should.have.been.calledOnce;
                 cb.should.have.been.calledWith(null, [1]);
                 done();
             });
 
             flow.map([1], func, cb);
-
-            func.should.have.been.calledOnce;
         });
 
         it('Should call func once & get err in 1st val & call once cb with args err, 1', done => {
@@ -170,32 +187,30 @@ describe('Tests for flow.js', () => {
             });
 
             var cb = sinon.spy(() => {
+                func.should.have.been.calledOnce;
                 cb.should.have.been.calledOnce;
                 cb.should.have.been.calledWith(testErr, 1);
                 done();
             });
 
             flow.map([1, 2], func, cb);
-
-            func.should.have.been.calledOnce;
         });
 
         it('Should call func thrice & call cb once with arguments null, [1, 2, 3]', done => {
             var func = sinon.spy((data, cb) => {
-                setTimeout(() => {
+                setImmediate(() => {
                     cb(null, data);
-                }, 500);
+                });
             });
 
             var cb = sinon.spy(() => {
+                func.should.have.been.calledThrice;
                 cb.should.have.been.calledOnce;
                 cb.should.have.been.calledWith(null, [1, 2, 3]);
                 done();
             });
 
             flow.map([1, 2, 3], func, cb);
-
-            func.should.have.been.calledThrice;
         });
     });
 });
